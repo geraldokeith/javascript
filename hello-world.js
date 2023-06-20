2099aa23242d45c8a7d0dd4805d36141080df5ff
gerald agonzebal <geraldo2keith@gmail.com>
2023-06-20 12:15:13 -0700

    javascript files

---
+++ hello-world.js
@@ -0,0 +1,59 @@
+console.log("hello world")
+
+//1.comments
+//used to provide information about the code
+// can be used in general to provide any information or suggestions
+// makes it easy to understand the code
+// can also be used to comment out code sections
+// comments are ignored by javascript engines
+// comments can be multiline or single line
+// examples
+// this is a single line comment
+/*
+this is a multiline comment
+*/ 
+
+// 2. variables
+//  used to store data 
+// can  be looked at as containers for storing data values
+// must contain only letters, digits or $ and _ symbol
+
+// guidelines for naming variables
+// must begin with a letter or_
+// should not start with a number
+// must not be a reserved key word like if, else, boolen, let, var, const etc
+// they are case sensetive
+// use meaningful names
+
+
+// var was used in earlier versions but now we use let and const
+
+// examples
+// variable declaration
+let age;
+// variable initialisation
+age = 23
+// = this is an assignment operator
+let studentName = "monica"
+console.log(studentName)
+// declaring many variables on the same line
+let firstName, lastName, cohort, year
+firstName = "edwin";
+lastName = "agonzebwa"
+cohort = 5
+year = 2023
+ 
+console.log(lastName)
+// dealing with constants
+const carName = "subaru"
+console.log(carName)
+// carName = "Audi"
+// console.log(carName)
+Const carNumber
+
+// naming coventions
+// 1. camelcase eg . firstName
+// 2. pascalcase eg. FirstName
+// 3. snakecase eg. first_name
+// 4. all caps snakecase eg. FIRST_NAME
+// Kebabcase eg. first-name
